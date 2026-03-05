fcefe930bb6415a21098cd783ff3d6d89f78e1c4
MWillian <matheuswno02@gmail.com>
2026-03-05 20:14:53 -0300

    Criando estrutura inicial do projeto

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-import {express} from 'express';
+import express from 'express';
+import usuariosRouter from './routes/eventos.routes.js';
+
+const app = express();
+app.use(express.json());
+app.use('/eventos',usuariosRouter);
+
+const PORTA =   process.env.PORTA || 3000;
+app.listen(PORTA);
